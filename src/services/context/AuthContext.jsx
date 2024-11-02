bbd4b34a243297949a1e8417d0a0a3709cdf5602
aagarcia-se <angelgarciap.dev@gmail.com>
2024-11-01 20:41:40 -0600

    lock

--- src/services/context/AuthContext.jsx
+++ src/services/context/AuthContext.jsx
@@ -5,53 +5,53 @@ import { GuardarDiasAcreditados } from "../VacationApp/Historial/SetearDias.serv
 
 const AuthContext = createContext();
 
-const AuthProvider = ({ children }) => {
-  const [userData, setUserData] = useState({});
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+// const AuthProvider = ({ children }) => {
+//   const [userData, setUserData] = useState({});
+//   const [loading, setLoading] = useState(false);
+//   const [error, setError] = useState(null);
 
-  const authenticate = async (username, password) => {
-    setLoading(true);
-    setError(null);
+//   const authenticate = async (username, password) => {
+//     setLoading(true);
+//     setError(null);
 
-    try {
-      const response = await login(username, password);
-      const userData = response.userLogin.userData;
-      setUserData(userData);
-      localStorage.setItem("userData", JSON.stringify(userData));
+//     try {
+//       const response = await login(username, password);
+//       const userData = response.userLogin.userData;
+//       setUserData(userData);
+//       localStorage.setItem("userData", JSON.stringify(userData));
 
-      //Aqui colocamos todo lo que queremos que se ejecute en segundo plano al iniciar sesion.
-      //---------------------------------------------------------------------------------------
-
-
-      // Ejecuta `GuardarDiasAcreditados` en segundo plano
-      GuardarDiasAcreditados(userData); // Llamada sin `await`
+//       //Aqui colocamos todo lo que queremos que se ejecute en segundo plano al iniciar sesion.
+//       //---------------------------------------------------------------------------------------
 
 
+//       // Ejecuta `GuardarDiasAcreditados` en segundo plano
+//       GuardarDiasAcreditados(userData); // Llamada sin `await`
 
-      //---------------------------------------------------------------------------------------
+
+
+//       //---------------------------------------------------------------------------------------
       
-    } catch (err) {
-      if (err && err.message && err.response.status === 500) {
-        setError("Servicio no disponible, intente más tarde");
-      } else if (err && err.response.data.userLogin.codErr) {
-        setError("Usuario o contraseña incorrectos. Por favor, inténtalo de nuevo.");
-      } else {
-        setError("Ha ocurrido un error");
-      }
-    } finally {
-      setLoading(false);
-    }
-  };
+//     } catch (err) {
+//       if (err && err.message && err.response.status === 500) {
+//         setError("Servicio no disponible, intente más tarde");
+//       } else if (err && err.response.data.userLogin.codErr) {
+//         setError("Usuario o contraseña incorrectos. Por favor, inténtalo de nuevo.");
+//       } else {
+//         setError("Ha ocurrido un error");
+//       }
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
 
-  const logout = () => {
-    setUserData(null);
-    localStorage.removeItem("userData");
-  };
+//   const logout = () => {
+//     setUserData(null);
+//     localStorage.removeItem("userData");
+//   };
 
-  const data = { logout, userData, authenticate, loading, error };
-  return <AuthContext.Provider value={data}>{children}</AuthContext.Provider>;
-};
+//   const data = { logout, userData, authenticate, loading, error };
+//   return <AuthContext.Provider value={data}>{children}</AuthContext.Provider>;
+// };
 
 export { AuthProvider };
 export default AuthContext;
