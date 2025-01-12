1d1eb8671ce6490ca77eec75b3d48bf23afb00ac
aagarcia-se <angelgarciap.dev@gmail.com>
2025-01-12 12:47:39 -0600

    Agregar configuracion de entorno

--- src/config/enviroment.js
+++ src/config/enviroment.js
@@ -4,4 +4,4 @@
 
 
 //export const API_URL = "http://localhost:3000/api" //Desarrollo
-export const API_URL = "https://control-de-vaciones-api.vercel.app/" //Produccion
+export const API_URL = "https://control-de-vaciones-api.vercel.app/api" //Produccion
